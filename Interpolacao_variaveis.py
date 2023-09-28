cbc339b57e667a4ae8289429d859a5c2f923b9fa
Daniele Gutinik <danielegutinik@gmail.com>
2023-09-28 20:31:26 -0300

    Create Interpolacao_variaveis.py
    Aula  03 Git

---
+++ Interpolacao_variaveis.py
@@ -0,0 +1,34 @@
+nome = "XXXXXXXXXXXXX"
+idade = xx
+profissao = "xxxxxxxxxxxx" 
+linguagem = "Python"
+
+saldo = 10245.435
+
+#dados = {"nome:": "XXXXXXXXXXXXX", "idade":XX}
+
+
+print("nome: %s Idade: %d " % (nome, idade))
+
+print("nome: {} Idade: {}". format (nome, idade))
+
+print("nome: {1} Idade: {0}".format(idade, nome))
+
+
+print("Nome:{}  Idade:{} ".format(idade, nome))
+print("Nome:{0} Idade:{1} Nome: {1} {1}".format(idade, nome))
+
+
+print("Nome: {nome} Idade:{idade}".format(nome=nome, idade=idade))
+
+
+# Criar um reposito client na sua maquina;
+# Colocar o codigo fonte disponibilizado na aula;
+# Em seguida subir para o Guthub;
+# Onde este o XXX colocar seu dados pessoais, no fonte que esta sua maquina;
+# Aguarde a segunda aula;
+
+
+
+
+
